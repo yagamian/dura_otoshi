586fef3f04f732faf61a54964f04cd1630023efe
orisano <owan.orisano@gmail.com>
2017-04-21 20:47:10 +0900

    refactor: 外のスコープを汚さないように変更

--- katsura_fall.js
+++ katsura_fall.js
@@ -1,24 +1,25 @@
-var width = window.innerWidth;
+(function () {
+    'use strict';
 
+    window.addEventListener('DOMContentLoaded', function start() {
+        var $fall = document.getElementById('fall');
 
-window.onload = function start() {
+        var anim = TweenMax.to('#katsura', 2, {
+            x: function() {
+                return (document.body.clientWidth - 200);
+            },
+            ease: 'linear',
+            repeat: -1,
+            yoyo: true
+        });
 
-    anim = TweenMax.to('#katsura', 2, {
-        x: function(){
-            return (document.body.clientWidth - 200);
-        }, 
-        ease: 'linear',
-        repeat: -1,
-        yoyo: true
-    });
-}
-
-function fall(){
-    var fall = document.getElementById('fall');
-    fall.disabled--;
-    anim.pause();
-    anim = TweenMax.to('#katsura', 1, {
-        y: '+= 225',
-        ease: 'linear'
-    });
-}
+        $fall.addEventListener('click', function () {
+            $fall.disabled--;
+            anim.pause();
+            anim = TweenMax.to('#katsura', 1, {
+                y: '+= 225',
+                ease: 'linear'
+            });
+        }, false);
+    }, false);
+})();
