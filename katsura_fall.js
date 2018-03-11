8218b88798ed32c0b51bc4a21711691c6b48ba8b
nve3pd <uchihara1229@gmail.com>
2018-03-11 22:17:25 +0900

    [fix] mergeの時におかしくなっていたっぽいので直した

--- katsura_fall.js
+++ katsura_fall.js
@@ -1,29 +1,25 @@
 (function () {
-
-    "use strict";
+    'use strict';
 
     window.addEventListener('DOMContentLoaded', function start() {
         var $fall = document.getElementById('fall');
 
-    window.onload = function start() {
-
-        anim = TweenMax.to('#katsura', 2, {
-            x: function(){
+        var anim = TweenMax.to('#katsura', 2, {
+            x: function() {
                 return (document.body.clientWidth - 200);
-            }, 
+            },
             ease: 'linear',
             repeat: -1,
             yoyo: true
         });
-    }
 
-    function fall(){
-        var fall = document.getElementById('fall');
-        fall.disabled--;
-        anim.pause();
-        anim = TweenMax.to('#katsura', 1, {
-            y: '+= 225',
-            ease: 'linear'
-        });
-    }
+        $fall.addEventListener('click', function () {
+            $fall.disabled = true;
+            anim.pause();
+            anim = TweenMax.to('#katsura', 1, {
+                y: '+= 225',
+                ease: 'linear'
+            });
+        }, false);
+    }, false);
 })();
